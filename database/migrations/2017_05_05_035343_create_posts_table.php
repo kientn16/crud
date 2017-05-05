e1629ca456aaaf9280484bb15608e8b8b707e862
kientn <kientn@rikkeisoft.com>
2017-05-05 16:42:06 +0700

    First Commit

---
+++ database/migrations/2017_05_05_035343_create_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::defaultStringLength(191);
+        Schema::create('posts', function(Blueprint $table) {
+            $table->charset = 'utf8';
+            $table->collation = 'utf8_unicode_ci';
+            $table->increments('id');
+            $table->string('title');
+            $table->text('content');
+            $table->string('category');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('posts');
+    }
+}
